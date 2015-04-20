43e1143faff8be232e18739e22036d96edd70e3e
Daniel Ramírez <dramireztorres@gmail.com>
2015-04-20 14:17:30 +0200

    Adding PIP configuration

--- setup.py
+++ setup.py
@@ -1,15 +1,25 @@
 import os
 from setuptools import setup
  
-README = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
+README = open(os.path.join(os.path.dirname(__file__), 'README.md')).read()
  
 # Allow setup.py to be run from any path
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
  
 setup(
+<<<<<<< HEAD
     name = 'athentoimaging',
     version = '0.1',
     packages = ['athentoimaging'],
+=======
+    name = 'athento-imaging',
+    version = '0.1.8',
+    packages = ['athento-imaging'],
+    install_requires =  [
+        'argparse',
+        'numpy'
+    ],
+>>>>>>> Adding PIP configuration
     include_package_data = True,
     license = 'Athento Shared Source License',
     description = 'Image cleaning and OCR improvement package in Python using OpenCV.',
@@ -18,10 +28,10 @@ setup(
     author = 'Daniel Ramirez Torres, Jose Luis de la Rosa',
     author_email = 'dramireztorres@gmail.com, jlr@athento.com',
     classifiers =[
-        'Development Status :: 3 - Alpha'
+        'Development Status :: 3 - Alpha',
         'Environment :: Other Environment',
         'Intended Audience :: Developers',
-        'License :: Athento Shared Source License 1.0',
+        'License :: Other/Proprietary License',
         'Operating System :: POSIX :: Linux',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.7',
